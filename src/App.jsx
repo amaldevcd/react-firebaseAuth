2c21bc5123d5f69902478de1913b473cf1c1f2ff
AMAL <mailtoamaldevcd@gmail.com>
2022-12-17 01:15:44 +0530

    img is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { auth,provider } from './config'
-import { signInWithPopup } from 'firebase/auth'
-
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { auth, provider } from "./config";
+import { signInWithPopup } from "firebase/auth";
 
 function App() {
-  const [value,setValue] = useState('');
-  const handleClick = () =>
-  {
-    signInWithPopup(auth,provider).then((data)=>{
-      setValue(data.user.email)
-      localStorage.setItem("email",data.user.email);
-    })
-  }
+  const [value, setValue] = useState("");
+  const [img,setImg] = useState("");
+  const handleClick = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      setValue(data.user.displayName);
+      console.log(data.user.photoURL);
+      localStorage.setItem("name", data.user.displayName)
+      localStorage.setItem("img",data.user.photoURL);
+    });
+  };
 
-  useEffect(()=>{
-    setValue(localStorage.getItem("email"))
-  })
+  useEffect(() => {
+    setValue(localStorage.getItem("name"));
+    setImg(localStorage.getItem("img"));
+  });
 
-  const logout = () =>
-  {
-    localStorage.clear()
+  const logout = () => {
+    localStorage.clear();
     window.location.reload();
-  }
+  };
   return (
-  <div>
-    {value?<div><h2>Welcome {value}</h2>
-    <button onClick={logout}>Logout</button>
-    </div>:
-      <button onClick={handleClick}>Sign in With Google</button>}
-  </div>
-  )
+    <div>
+      {value ? (
+        <div>
+          <h2>Welcome {value}</h2>
+          <img src={img} alt="Avatar" style={{width:"200px",borderRadius:"50%"}}></img><br/>
+          <button onClick={logout}>Logout</button>
+        </div>
+      ) : (
+        <button onClick={handleClick}>Sign in With Google</button>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
